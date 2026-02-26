4b32cdb3407bbb6917d128a4975d3f6a7a27968e
Kat R. <56617952+KxttyKxt@users.noreply.github.com>
2026-02-26 12:52:50 -0500

    Add swem seeds to seed pouch items

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,4 +1,6 @@
 ServerEvents.recipes(event => {
+    // book + saddle = Mythical Mounts guidebook
+    // the output requires NBT data, so this recipe would not work in a datapack.
 	event.shapeless(
         Item.of(
             'patchouli:guide_book', 
