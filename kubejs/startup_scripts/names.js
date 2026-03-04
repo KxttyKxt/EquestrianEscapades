a36e69a219f82fd00fbe38a908cab52977075a95
Kat R. <56617952+KxttyKxt@users.noreply.github.com>
2026-03-04 13:55:43 -0500

    Always show icons for livestock/horses
    And always show names for mooblooms because they use the cow icon

--- kubejs/startup_scripts/names.js
+++ kubejs/startup_scripts/names.js
@@ -1,1 +1,4 @@
-Platform.mods.escapades.name = "Equestrian Escapades";
+// Wanted to associate the `escapades` namespace with the below name,
+//  but since it's not an actual mod, this script will fail.
+
+//Platform.mods.escapades.name = "Equestrian Escapades";
